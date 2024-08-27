e27aed61370d80184add90de5e7081391919e9d5
Jones <jamil.jouj@iths.se>
2024-08-27 16:36:15 +0200

    EvenAndOdd

---
+++ 03IfElse/03IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int number;
+Console.WriteLine("Input a number: ");
+
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("The number is Even.");
+    return;
+}
+
+Console.WriteLine("The number is Odd.");
